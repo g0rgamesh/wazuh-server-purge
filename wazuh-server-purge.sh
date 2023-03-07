b7118445f8ac931cdc2bb1939882eceee1e9e8a8
Rafal Lewandowski <52123760+g0rgamesh@users.noreply.github.com>
2023-03-07 15:14:54 +0100

    removed progress bars

--- wazuh-server-purge.sh
+++ wazuh-server-purge.sh
@@ -9,13 +9,6 @@ rm -rf /var/lib/wazuh-dashboard/ &&
 rm -rf /usr/share/wazuh-dashboard/ &&
 rm -rf /etc/wazuh-dashboard/ &&
 systemctl disable wazuh-dashboard &&
-systemctl daemon-reload &&
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 echo "Removed wazuh dashboard."
 
 # Remove server
@@ -25,12 +18,6 @@ rm -rf /var/ossec/ &&
 echo "Disabling wazuh-manager service"
 systemctl disable wazuh-manager &&
 systemctl daemon-reload &&
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 echo "Removed wazuh server."
 
 # Remove filebeat
@@ -41,12 +28,6 @@ rm -rf /usr/share/filebeat/ &&
 rm -rf /etc/filebeat/ &&
 systemctl disable filebeat.service &&
 systemctl daemon-reload &&
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 echo "Removed filebeat."
 
 # Remove indexer
@@ -55,11 +36,5 @@ yum remove wazuh-indexer -y &&
 rm -rf /var/lib/wazuh-indexer/ &&
 rm -rf /usr/share/wazuh-indexer/ &&
 rm -rf /etc/wazuh-indexer/ &&
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 echo -e "Removed wazuh indexer.\n"
 echo "All wazuh server components removed successfully.
