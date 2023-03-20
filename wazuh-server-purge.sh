0ccfa99302772b284037ebaed21ba0e97a522482
Rafal Lewandowski <rafal@stackhpc.com>
2023-03-20 12:12:06 +0100

    fs bypass

--- wazuh-server-purge.sh
+++ wazuh-server-purge.sh
@@ -13,9 +13,9 @@ systemctl daemon-reload
 echo -e "\e[36;5;82mRemoving wazuh-dashboard and files...\e[0m"
 if $(which yum 2>/dev/null >/dev/null); then
     yum remove wazuh-dashboard -y
-    rm -rf /var/lib/wazuh-dashboard/
-    rm -rf /usr/share/wazuh-dashboard/
-    rm -rf /etc/wazuh-dashboard/
+    rm -rf /var/lib/wazuh-dashboard/*
+    rm -rf /usr/share/wazuh-dashboard/*
+    rm -rf /etc/wazuh-dashboard/*
 else
     apt-get remove --purge wazuh-dashboard -y
 fi
@@ -43,9 +43,9 @@ systemctl daemon-reload
 echo -e "\e[36;5;82mRemoving filebeat and files...\e[0m"
 if $(which yum 2>/dev/null >/dev/null); then
     yum remove filebeat -y
-    rm -rf /var/lib/filebeat/
-    rm -rf /usr/share/filebeat/
-    rm -rf /etc/filebeat/
+    rm -rf /var/lib/filebeat/*
+    rm -rf /usr/share/filebeat/*
+    rm -rf /etc/filebeat/*
 else
     apt-get remove --purge filebeat -y
 fi
@@ -59,9 +59,9 @@ systemctl daemon-reload
 echo -e "\e[36;5;82mRemoving wazuh-indexer and files...\e[0m"
 if $(which yum 2>/dev/null >/dev/null); then
     yum remove wazuh-indexer -y
-    rm -rf /var/lib/wazuh-indexer/
-    rm -rf /usr/share/wazuh-indexer/
-    rm -rf /etc/wazuh-indexer/
+    rm -rf /var/lib/wazuh-indexer/*
+    rm -rf /usr/share/wazuh-indexer/*
+    rm -rf /etc/wazuh-indexer/*
 else
     apt-get remove --purge wazuh-indexer -y
 fi
